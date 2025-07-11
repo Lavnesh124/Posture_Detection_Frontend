33810b7e647c8e01c87cfaa8ac14ccc6e648dcd7
Lavnesh yadav <lavneshy@gmail.com>
2025-07-11 16:12:48 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = https://posture-detection-backend-1.onrender.com;
 
 const videoConstraints = {
   width: 640,
